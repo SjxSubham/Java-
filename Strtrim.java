48468fab33473f6eae3890db74b8ace65b7d41e9
SjxSubham <sjxsubham@gmail.com>
2024-03-04 15:40:57 +0530

    FSP java 2k24

---
+++ Strtrim.java
@@ -0,0 +1,9 @@
+public class Strtrim {
+    public static void main(String args[]){ 
+        String Str = new String("Welcome Momo     ");
+        System.out.println("returned value: ");
+
+        System.out.println(Str.trim());
+    }
+}
+// it is used to eliminate any leading or trailing spaces in string 
---
+++ Uppercase.java
@@ -0,0 +1,9 @@
+public class Uppercase {
+    public static void main(String args[]){
+        String Str = new String("MomO baby");
+        System.out.println("return Value:");
+    
+        System.out.println(Str.toUpperCase());
+        
+        }
+}
